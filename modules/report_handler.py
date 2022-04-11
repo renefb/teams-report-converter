a981427ffb206676b15a0e16516f8fc57929dbdf
Renê Fernandes <renefb@protonmail.com>
2022-04-11 12:34:55 -0300

    defining frequency calc method

--- modules/report_handler.py
+++ modules/report_handler.py
@@ -33,28 +33,26 @@ class TeamsAttendeeEngagementReportHandler:
     
     def __load_csv(self):
         df = pd.read_csv(self.__report_content, parse_dates=['UTC Event Timestamp'])
-        df['UTC Event Timestamp'] = df['UTC Event Timestamp'].apply(lambda x: pytz.timezone('GMT').localize(x))
-        df = df.sort_values(by=['UTC Event Timestamp'])
+        df.columns = ['SessionId', 'ParticipantId', 'FullName', 'UserAgent', 'UtcEventTimestamp', 'Action', 'Role']
+        df['UtcEventTimestamp'] = df['UtcEventTimestamp'].apply(lambda x: pytz.timezone('GMT').localize(x))
+        df = df.sort_values(by=['UtcEventTimestamp'])
         return df
         
         
     
     def __filter_by_action(self, action):
-        action_timestamp_column = f'UTC {action} Timestamp'
-        action_sess = self.__df.query('Action==@action').rename(columns={'UTC Event Timestamp': action_timestamp_column})
         keep_param = 'first' if action=="Joined" else 'last'
-        action_sess = action_sess.drop_duplicates(subset='Session Id', keep=keep_param)
-        action_sess = action_sess.set_index('Session Id')
-        target_cols = ['Participant Id', 'Full Name', 'UserAgent', 'Role', action_timestamp_column]
-        return action_sess[target_cols]
+        
+        action_sess = self.__df.query('Action==@action').rename(columns={'UtcEventTimestamp': f'{action}At'})
+        action_sess = action_sess.drop_duplicates(subset='SessionId', keep=keep_param)
+        action_sess = action_sess.drop(columns='Action').set_index('SessionId')
+        
+        return action_sess
  
     
     def __print_summary(self):
         width = 50
         summary = {
-            # 'header': {
-            #     'S U M M A R Y': '',
-            # },
             'start_end': {
                 'Informed event start': self.__event_start,
                 'Informed event end': self.__event_end
@@ -64,22 +62,22 @@ class TeamsAttendeeEngagementReportHandler:
             },
             'join': {
                 'Joined rows': self.__joined_df.shape[0],
-                ' - First at': self.__joined_df['UTC Joined Timestamp'].min(),
-                ' - Last at': self.__joined_df['UTC Joined Timestamp'].max(),
+                ' - First at': self.__joined_df['JoinedAt'].min(),
+                ' - Last at': self.__joined_df['JoinedAt'].max(),
             },
             'left': {
                 'Left rows': self.__left_df.shape[0],
-                ' - First at': self.__left_df['UTC Left Timestamp'].min(),
-                ' - Last at': self.__left_df['UTC Left Timestamp'].max()
+                ' - First at': self.__left_df['LeftAt'].min(),
+                ' - Last at': self.__left_df['LeftAt'].max()
             },
             'sessions': {
-                'Unique sessions': len(self.__df['Session Id'].unique()),
-                ' - Left before event start': (self.__sessions['Session Validation']=='Left early').sum(),
-                ' - Joined after event end': (self.__sessions['Session Validation']=='Joined late').sum(),
-                ' - Without Participant Id': self.__sessions['Participant Id'].isnull().sum(),
+                'Unique sessions': len(self.__df['SessionId'].unique()),
+                ' - Left before event start': (self.__sessions['Validation']=='Left early').sum(),
+                ' - Joined after event end': (self.__sessions['Validation']=='Joined late').sum(),
+                ' - Without Participant Id': self.__sessions['ParticipantId'].isnull().sum(),
             },
             'participants': {
-                'Unique participants': len(self.__sessions['Participant Id'].unique())
+                'Unique participants': len(self.__sessions['ParticipantId'].unique())
             }
         }
 
@@ -94,83 +92,75 @@ class TeamsAttendeeEngagementReportHandler:
     
         
     def __pair_sessions(self):
-        paired_sess = pd.concat([self.__joined_df, self.__left_df['UTC Left Timestamp']], axis=1)
+        paired_sess = pd.concat([self.__joined_df, self.__left_df['LeftAt']], axis=1)
         
-        is_join_late = paired_sess['UTC Joined Timestamp'].apply(lambda x: True if x >= self.__event_end else False)
-        is_leaving_early = paired_sess['UTC Left Timestamp'].apply(lambda x: True if x <= self.__event_start else False)
+        is_join_late = paired_sess['JoinedAt'].apply(lambda x: True if x >= self.__event_end else False)
+        is_leaving_early = paired_sess['LeftAt'].apply(lambda x: True if x <= self.__event_start else False)
         
-        paired_sess['Session Validation'] = 'Valid'
-        paired_sess.loc[is_join_late,'Session Validation'][is_join_late] = 'Joined late'
-        paired_sess.loc[is_leaving_early, 'Session Validation'] = 'Left early'
+        paired_sess['Validation'] = 'Valid'
+        paired_sess.loc[is_join_late,'Validation'][is_join_late] = 'Joined late'
+        paired_sess.loc[is_leaving_early, 'Validation'] = 'Left early'
+
+        paired_sess['TruncJoinedAt'] = paired_sess['JoinedAt'].apply(lambda x: x if x>self.__event_start else self.__event_start)
+        paired_sess['TruncLeftAt'] = paired_sess['LeftAt'].apply(lambda x: x if x<self.__event_end else self.__event_end)
+
+        paired_sess.loc[is_join_late,'Validation'] = 'Joined late'
+        paired_sess.loc[is_join_late,'TruncJoinedAt'] = self.__event_end
+        paired_sess.loc[is_leaving_early, 'Validation'] = 'Left early'
+        paired_sess.loc[is_leaving_early, 'TruncLeftAt'] = self.__event_start
         
         return paired_sess
 
 
     def __calculate_frequency(self):
         participants_sess = self.__sessions.copy()
-        participants_sess = participants_sess[~participants_sess['Participant Id'].isnull()]
-        participants_sess = participants_sess[participants_sess['Session Validation']=='Valid']
+        participants_sess = participants_sess[~participants_sess['ParticipantId'].isnull()]
+        participants_sess = participants_sess[participants_sess['Validation']=='Valid']
         
-        participants_ids = participants_sess['Participant Id'].unique()
-        participants_sess = participants_sess.set_index('Participant Id')
+        participants_ids = participants_sess['ParticipantId'].unique()
+        participants_sess = participants_sess.set_index('ParticipantId')
 
-        target_columns = ['Full Name', 'Role', 'UTC Joined Timestamp', 'UTC Left Timestamp']
+        target_columns = ['FullName', 'Role', 'JoinedAt', 'LeftAt']
         participants_sess = participants_sess[target_columns]
-        participants_sess['Session Duration'] = participants_sess['UTC Left Timestamp'] - participants_sess['UTC Joined Timestamp']
+        participants_sess['Duration'] = participants_sess['LeftAt'] - participants_sess['JoinedAt']
         
         freq = []
 
         for id in participants_ids:
-            participant = {'Participant Id': id}
-            participant_freq = participants_sess.loc[id]
-            if type(participant_freq)==pd.Series:
-                print('\tis a series')
-                participant_freq = participant_freq.to_frame()
+            participant_slice = participants_sess.loc[id]
+            if type(participant_slice)==pd.DataFrame:
+                participant_slice = self.__merge_sessions(participant_slice)
 
-            fields_to_drop = ['UTC Joined Timestamp', 'UTC Left Timestamp']
+            participant_slice = participant_slice.drop(['JoinedAt', 'LeftAt'])
 
-            print(f'Participant {id} has {participant_freq.shape[0]} valid sessions')
-            if participant_freq.shape[0]==1:
-                print('\tsingle session')
-                participant.update(participant_freq.drop(fields_to_drop).to_dict())
-            else:
-                print('\tmultiple sessions')
-                participant = self.__merge_sessions(participant_freq)
-                participant = participant.drop(fields_to_drop).to_dict()
-            freq.append(participant)
+            freq.append(participant_slice)
 
         return pd.DataFrame(freq)
     
 
     def __merge_sessions(self, df):
-        # ['Participant Id']['Full Name', 'Role', 'UTC Joined Timestamp', 'UTC Left Timestamp', 'Session Duration']
-        print('\tentering into merge sessions:')
-        print(df)
         session = df.iloc[0]
-        print('\t', type(session))
-        joined = session['UTC Joined Timestamp']
-        left = session['UTC Left Timestamp']
-        duration = session['Session Duration']
+        joined = session['JoinedAt']
+        left = session['LeftAt']
+        duration = session['Duration']
         duration_arr = []
 
-        print('\tentering iterrows')
         for i, row in df.iloc[1:].iterrows():
-            if row['UTC Joined Timestamp'] < left:
-                if row['UTC Left Timestamp'] <= left:
+            if row['JoinedAt'] < left:
+                if row['LeftAt'] <= left:
                     pass
                 else:
-                    left = row['UTC Left Timestamp']
+                    left = row['LeftAt']
                     duration = left - joined
             else:
                 duration_arr.append(duration)
-                joined = row['UTC Joined Timestamp']
-                left = row['UTC Left Timestamp']
+                joined = row['JoinedAt']
+                left = row['LeftAt']
                 duration = left - joined
-        print('\texiting iterrows\n----------------------------')
 
         duration_arr.append(duration)
         duration_sum = np.array(duration_arr).sum()
-        session.loc['Session Duration'] = duration_sum
+        session.loc['Duration'] = duration_sum
 
         return session
 
