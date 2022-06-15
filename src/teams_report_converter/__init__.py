904eefd7ea7aab16b45f6b3f4f58d1f4d1caa3c4
Renê <renefb@protonmail.com>
2022-06-15 07:59:31 -0300

    downgrade for compatibility with py3.6.x; changed to MIC license

--- src/teams_report_converter/__init__.py
+++ src/teams_report_converter/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 
 # Version of package
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # Importing main class
 from .report_handler import TeamsAttendeeEngagementReportHandler as Converter
