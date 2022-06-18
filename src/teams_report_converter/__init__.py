2e68f66fdace78180d9a4e274cc6819f895fb798
Renê <renefb@protonmail.com>
2022-06-18 09:24:40 -0300

    .

--- src/teams_report_converter/__init__.py
+++ src/teams_report_converter/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 
 # Version of package
-__version__ = "0.2.1"
+__version__ = "0.2.3"
 
 # Importing main class
 from .report_handler import TeamsAttendeeEngagementReportHandler as Converter
